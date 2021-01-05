f93a61a3b44510ad6a7480e5ad998bcf2cd2de7a
Oleksandr Zhezdorf <aleks@gmail.com>
2021-01-05 23:45:05 +0200

    Understanding closures

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function curry(...theArgs) {
         return previous + current;
     }, 0);
     const sumFunction = function () {
-        for (var i = 0, j = arguments.length; i < j; i++) {
+        for (let i = 0, j = arguments.length; i < j; i++) {
             sum += arguments[i];
         }
         return sum;
@@ -14,8 +14,25 @@ function curry(...theArgs) {
     return sumFunction;
 }
 
-const curriedFunction = curry(1, 2);
-console.log(curriedFunction(3, 4));
+const curriedFunction = curry(1, 2, 4);
+console.log(curriedFunction(3, 4, 5));
+
+//////
+
+function sum2(...theArgs) {
+    return theArgs.reduce((previous, current) => {
+        return previous + current;
+    }, 0);
+}
+
+function curry2(fn, ...args) {
+    return (..._arg) => {
+        return fn(...args, ..._arg);
+    }
+};
+
+const curriedFunction2 = curry2(sum2, 1, 2);
+console.log(curriedFunction2(3, 4))
 
 
 /* Задача 2 */
@@ -35,11 +52,13 @@ function counter() {
     };
 }
 
-var iterator = counter();
+let iterator = counter();
 iterator.inc();
 iterator.inc();
 iterator.dec();
 console.log(iterator.value());
+
+
 
 
 
